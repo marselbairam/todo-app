1679fc8a7330948bf2752bc700297aa34a401d94
Marsel <marselbairam@gmail.com>
2020-07-27 15:50:03 +0300

    make sidebar basics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
 import React from 'react';
+import TagsList from './components/TagsList/TagsList';
+
+import { ReactComponent as ListSvg } from './assets/img/list.svg';
 
 function App() {
   return (
-    <div className="App">
+    <div className="todo">
+      <div className="todo__sidebar">
+        <TagsList items={[
+          {
+            active: true,
+            icon: <ListSvg className="tags-list__icon" />,
+            name: 'All tasks'
+          }
+        ]} />
+        <TagsList items={[
+          {
+            color: 'mint',
+            name: 'Purchases'
+          },
+          {
+            color: 'blue',
+            name: 'Frontend'
+          },
+          {
+            color: 'pink',
+            name: 'Movies and TV series'
+          },
+          {
+            color: 'celadon',
+            name: 'Books'
+          },
+          {
+            color: 'gray',
+            name: 'Personal'
+          }
+        ]} />
+      </div>
+      <div className="todo__tasks"></div>
     </div>
   );
 }
