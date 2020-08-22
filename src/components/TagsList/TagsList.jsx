967a914a6545ac7b7af2cfe941d16e94951e4796
Marsel <marselbairam@gmail.com>
2020-08-22 20:18:16 +0300

    add remove btn, upd style

--- src/components/TagsList/TagsList.jsx
+++ src/components/TagsList/TagsList.jsx
@@ -5,7 +5,13 @@ import { ReactComponent as RemoveSvg } from '../../assets/img/remove.svg';
 
 import './TagsList.scss';
 
-const TagsList = ({ items, isRemovable, onClick }) => {
+const TagsList = ({ items, isRemovable, onClick, onRemove }) => {
+  const removeList = item => {
+    if (window.confirm('Вы действительно хотите удалить список?')) {
+      onRemove(item);
+    }
+  };
+
   return (
     <ul
       className="TagsList"
@@ -20,13 +26,16 @@ const TagsList = ({ items, isRemovable, onClick }) => {
             item.icon
           ) : (
             <Tag
-              className="TagsList__Icon"
+              className="TagsList__Icon TagsList__LeftIcon"
               color={item.color}
               size={item.size}
             />
           )}
           <span className="TagsList__Label">{item.name}</span>
-          {isRemovable && <RemoveSvg className="TagsList__Icon TagsList__Icon_Gray TagsList__Icon_Interactive TagsList__Icon_Hidden" />}
+          {isRemovable && <RemoveSvg
+            className="TagsList__Icon TagsList__Icon_Gray TagsList__Icon_Interactive TagsList__Icon_Hidden TagsList__RemoveBtn"
+            onClick={() => removeList(item)}
+          />}
         </li>
       ))}
     </ul>
