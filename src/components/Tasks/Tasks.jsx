d7f4041cebe509728ed8614b480fd5d222ef50af
Marsel <marselbairam@gmail.com>
2020-08-26 21:28:46 +0300

    make Tasks component

---
+++ src/components/Tasks/Tasks.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import './Tasks.scss';
+
+import {ReactComponent as CheckSvg} from '../../assets/img/check.svg';
+
+const Tasks = ({ list }) => {
+  return (
+    <div className="Tasks">
+      <h2 className="Tasks__title">
+        {list.name}
+      </h2>
+      <div className="Tasks__items">
+        {list.tasks.map(task => (
+          <div
+            key={task.id}
+            className="Tasks__item"
+          >
+            <label className="Control Control_checkbox">
+              <input className="Control__field" type="checkbox" />
+              <span className="Control__mark">
+              <CheckSvg className="Control__mark-icon" />
+            </span>
+            </label>
+            <input
+              className="Field Field_fullWidth"
+              type="text"
+              readOnly
+              value={task.text}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Tasks;
